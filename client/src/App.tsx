5bdbed71b32d8a15667acfb3f7ab7e3831876783
balu-cod <kodimurthybalu@gmail.com>
2026-01-23 12:34:16 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "./components/ui/toaster";
 import { Layout } from "@/components/Layout";
 
 import Home from "@/pages/Home";
